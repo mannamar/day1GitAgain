c306fa94ad3b1d306747739415be593a5d63c06f
Amardeep Mann <amar2slick@gmail.com>
2022-10-17 11:41:54 -0700

    Letting people know this is new

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // Just to double check we threw in this comment
+// New new new everything is new!
